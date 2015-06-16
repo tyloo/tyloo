94c04951c32dfdb3c33469bcbab1ec375a71fdbb
tyloo <jbonva@gmail.com>
2015-06-16 14:45:15 +0200

    Added typehinting Factory
    Signed-off-by: tyloo <jbonva@gmail.com>

--- tests/BasicTest.php
+++ tests/BasicTest.php
@@ -14,4 +14,52 @@ class BasicTest extends TestCase
         $this->visit('/')
              ->see('Julien \'Tyloo\' Bonvarlet');
     }
+
+    /**
+     * Test Menu Anchors : /resume
+     *
+     * @return void
+     */
+    public function testResumeAnchor()
+    {
+        $this->visit('/')
+             ->click('Resume')
+             ->seePageIs('/resume');
+    }
+
+    /**
+     * Test Menu Anchors : /works
+     *
+     * @return void
+     */
+    public function testWorksAnchor()
+    {
+        $this->visit('/')
+            ->click('Works')
+            ->seePageIs('/works');
+    }
+
+    /**
+     * Test Menu Anchors : /blog
+     *
+     * @return void
+     */
+    public function testBlogAnchor()
+    {
+        $this->visit('/')
+            ->click('Blog')
+            ->seePageIs('/blog');
+    }
+
+    /**
+     * Test Menu Anchors : contact
+     *
+     * @return void
+     */
+    public function testContactAnchor()
+    {
+        $this->visit('/')
+            ->click('Contact')
+            ->seePageIs('/contact');
+    }
 }
