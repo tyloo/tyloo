ad02fd1a2e88758cf5e9a139fe98aaf6e454d69e
tyloo <jbonva@gmail.com>
2015-07-09 16:30:21 +0200

    Changed Scrutinizer naming conf
    Signed-off-by: tyloo <jbonva@gmail.com>

--- app/Http/Controllers/Frontend/PagesController.php
+++ app/Http/Controllers/Frontend/PagesController.php
@@ -8,21 +8,43 @@ use App\Jobs\SendContactEmail;
 
 class PagesController extends Controller
 {
+    /**
+     * Home Page.
+     *
+     * @return \Illuminate\View\View
+     */
     public function home()
     {
         return view('frontend.pages.home');
     }
 
+    /**
+     * Resume Page.
+     *
+     * @return \Illuminate\View\View
+     */
     public function resume()
     {
         return view('frontend.pages.resume');
     }
 
+    /**
+     * Contact form.
+     *
+     * @return \Illuminate\View\View
+     */
     public function getContact()
     {
         return view('frontend.pages.contact');
     }
 
+    /**
+     * Send the Contact request.
+     *
+     * @param \App\Http\Requests\ContactRequest $request
+     *
+     * @return \Illuminate\View\View
+     */
     public function postContact(ContactRequest $request)
     {
         $this->dispatch(new SendContactEmail($request->all()));
