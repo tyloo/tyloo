41f168a9ff4b3dad5f349b52b2a0f047cb108237
tyloo <jbonva@gmail.com>
2015-07-09 16:16:51 +0200

    Fixed Perl-Style comments (Scritunizer)
    Signed-off-by: tyloo <jbonva@gmail.com>

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,70 +1,70 @@
 <?php
 
 Route::group(['namespace' => 'Frontend'], function () {
-    # Home
+    // Home
     Route::get('/', ['as' => 'home', 'uses' => 'PagesController@home']);
 
-    # Resume
+    // Resume
     Route::get('resume', ['as' => 'resume', 'uses' => 'PagesController@resume']);
 
-    # Contact
+    // Contact
     Route::get('contact', ['as' => 'contact', 'uses' => 'PagesController@getContact']);
     Route::post('contact', ['as' => 'contact', 'uses' => 'PagesController@postContact']);
 
-    # Search
+    // Search
     Route::get('search', ['as' => 'search', 'uses' => 'PagesController@search']);
 
-    # Blog
+    // Blog
     Route::get('blog', ['as' => 'blog.index', 'uses' => 'BlogController@index']);
     Route::get('blog/{slug}', ['as' => 'blog.show', 'uses' => 'BlogController@show']);
     Route::get('blog/tag/{slug}', ['as' => 'blog.tag', 'uses' => 'BlogController@tag']);
 
-    # Works
+    // Works
     Route::get('works', ['as' => 'works.index', 'uses' => 'WorkController@index']);
     Route::get('works/{slug}', ['as' => 'works.show', 'uses' => 'WorkController@show']);
 
-    # Authentication
+    // Authentication
     Route::get('auth/login', ['as' => 'auth.login', 'uses' => 'Auth\AuthController@getLogin']);
     Route::post('auth/login', ['as' => 'auth.login', 'uses' => 'Auth\AuthController@postLogin']);
     Route::get('auth/logout', ['as' => 'auth.logout', 'uses' => 'Auth\AuthController@getLogout']);
 
-    # Registration
+    // Registration
     Route::get('auth/register', ['as' => 'auth.register', 'uses' => 'Auth\AuthController@getRegister']);
     Route::post('auth/register', ['as' => 'auth.register', 'uses' => 'Auth\AuthController@postRegister']);
 
-    # Profile
+    // Profile
     Route::get('auth/profile', ['as' => 'auth.profile', 'uses' => 'Auth\ProfileController@index', 'middleware' => 'auth']);
 
-    # Password Reset Request
+    // Password Reset Request
     Route::get('auth/remind', ['as' => 'auth.remind', 'uses' => 'Auth\PasswordController@getEmail']);
     Route::post('auth/remind', ['as' => 'auth.remind', 'uses' => 'Auth\PasswordController@postEmail']);
 
-    # Password Reset
+    // Password Reset
     Route::get('auth/reset/{token}', ['as' => 'auth.reset', 'uses' => 'Auth\PasswordController@getReset']);
     Route::post('auth/reset', ['as' => 'auth.reset', 'uses' => 'Auth\PasswordController@postReset']);
 });
 
 Route::group(['namespace' => 'Backend', 'middleware' => 'auth', 'prefix' => 'admin'], function () {
-    # Dashboard
+    // Dashboard
     Route::get('/', ['as' => 'admin.dashboard', 'uses' => 'DashboardController@index']);
 
-    # Settings
+    // Settings
     Route::get('settings', ['as' => 'admin.settings.edit', 'uses' => 'SettingsController@edit']);
     Route::put('settings', ['as' => 'admin.settings.update', 'uses' => 'SettingsController@update']);
 
-    # Blog
+    // Blog
     Route::resource('blog', 'BlogController');
     Route::get('blog/delete/{id}', ['as' => 'admin.blog.destroy', 'uses' => 'BlogController@destroy']);
 
-    # Works
+    // Works
     Route::resource('works', 'WorksController');
     Route::get('works/delete/{id}', ['as' => 'admin.works.destroy', 'uses' => 'WorksController@destroy']);
 
-    # Users
+    // Users
     Route::resource('users', 'UsersController');
     Route::get('users/delete/{id}', ['as' => 'admin.users.destroy', 'uses' => 'UsersController@destroy']);
 
-    # Tags
+    // Tags
     Route::resource('tags', 'TagsController');
     Route::get('tags/delete/{id}', ['as' => 'admin.tags.destroy', 'uses' => 'TagsController@destroy']);
 });
