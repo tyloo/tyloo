3503c3d03bde2b5b4dfda88dfa57d7d4fd9df516
tyloo <jbonva@gmail.com>
2015-07-15 10:34:54 +0200

    Removed register shit
    Signed-off-by: tyloo <jbonva@gmail.com>

--- app/Http/Controllers/Backend/Auth/AuthController.php
+++ app/Http/Controllers/Backend/Auth/AuthController.php
@@ -35,36 +35,4 @@ class AuthController extends Controller
      * @var string
      */
     protected $loginPath = '/admin/auth/login';
-
-    /**
-     * Get a validator for an incoming registration request.
-     *
-     * @param array $data
-     *
-     * @return \Illuminate\Contracts\Validation\Validator
-     */
-    protected function validator(array $data)
-    {
-        return Validator::make($data, [
-            'name'     => 'required|max:255',
-            'email'    => 'required|email|max:255|unique:users',
-            'password' => 'required|confirmed|min:6',
-        ]);
-    }
-
-    /**
-     * Create a new user instance after a valid registration.
-     *
-     * @param array $data
-     *
-     * @return User
-     */
-    protected function create(array $data)
-    {
-        return User::create([
-            'name'     => $data['name'],
-            'email'    => $data['email'],
-            'password' => bcrypt($data['password']),
-        ]);
-    }
 }
