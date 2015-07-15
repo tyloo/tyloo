c749d3aff92d2102299afa4bf07cb06a505760f2
tyloo <jbonva@gmail.com>
2015-07-15 15:06:56 +0200

    Added Dashboard simple view
    Signed-off-by: tyloo <jbonva@gmail.com>

--- app/Http/Controllers/Backend/DashboardController.php
+++ app/Http/Controllers/Backend/DashboardController.php
@@ -3,11 +3,17 @@
 namespace App\Http\Controllers\Backend;
 
 use App\Http\Controllers\Controller;
+use App\Post;
+use App\User;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        return view('backend.dashboard.index');
+        $blogPostsCount = Post::where('type', 'blog')->count();
+        $workPostsCount = Post::where('type', 'work')->count();
+        $usersCount = User::all()->count();
+
+        return view('backend.dashboard.index', compact('blogPostsCount', 'workPostsCount', 'usersCount'));
     }
 }
--- resources/views/backend/dashboard/index.blade.php
+++ resources/views/backend/dashboard/index.blade.php
@@ -9,5 +9,62 @@
 @stop
 
 @section('content')
-    Dashboard!
+    <div class="row">
+        <div class="col-lg-3 col-xs-6">
+            <div class="small-box bg-aqua">
+                <div class="inner">
+                    <h3>{{ $blogPostsCount }}</h3>
+                    <p>Blog Posts</p>
+                </div>
+                <div class="icon">
+                    <i class="ion-social-wordpress-outline"></i>
+                </div>
+                <a href="{{ URL::route('admin.posts.index') }}" class="small-box-footer">
+                    More info <i class="fa fa-arrow-circle-right"></i>
+                </a>
+            </div>
+        </div>
+        <div class="col-lg-3 col-xs-6">
+            <div class="small-box bg-green">
+                <div class="inner">
+                    <h3>{{ $workPostsCount }}</h3>
+                    <p>Work Entries</p>
+                </div>
+                <div class="icon">
+                    <i class="ion ion-network"></i>
+                </div>
+                <a href="{{ URL::route('admin.posts.index') }}" class="small-box-footer">
+                    More info <i class="fa fa-arrow-circle-right"></i>
+                </a>
+            </div>
+        </div>
+        <div class="col-lg-3 col-xs-6">
+            <div class="small-box bg-yellow">
+                <div class="inner">
+                    <h3>XXX</h3>
+                    <p>Customers</p>
+                </div>
+                <div class="icon">
+                    <i class="ion ion-ios-people"></i>
+                </div>
+                <a href="#" class="small-box-footer">
+                    More info <i class="fa fa-arrow-circle-right"></i>
+                </a>
+            </div>
+        </div>
+        <div class="col-lg-3 col-xs-6">
+            <div class="small-box bg-red">
+                <div class="inner">
+                    <h3>{{ $usersCount }}</h3>
+                    <p>Registered Users</p>
+                </div>
+                <div class="icon">
+                    <i class="ion ion-pie-graph"></i>
+                </div>
+                <a href="{{ URL::route('admin.users.index') }}" class="small-box-footer">
+                    More info <i class="fa fa-arrow-circle-right"></i>
+                </a>
+            </div>
+        </div>
+    </div>
 @stop
