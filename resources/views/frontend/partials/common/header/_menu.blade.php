960bb70a8af0edb1ef9490d827dc7d95f1f8e0bc
tyloo <jbonva@gmail.com>
2015-07-23 15:25:42 +0200

    Removed Search and updated dependencies
    Signed-off-by: tyloo <jbonva@gmail.com>

--- resources/views/frontend/partials/common/header/_menu.blade.php
+++ resources/views/frontend/partials/common/header/_menu.blade.php
@@ -6,21 +6,21 @@
         <li{{ Request::segment(1) == 'blog/' ? ' class=active' : '' }}><a href="{{ URL::route('blog.index') }}">Blog</a></li>
 
         {{-- Search --}}
-        <li class="dropdown visible-lg-block">
-            <a href="#" class="dropdown-toggle dropdown-form-toggle" data-toggle="dropdown"><i class="fa fa-search"></i></a>
-            <ul class="dropdown-menu dropdown-menu-user">
-                <li id="dropdownForm">
-                    <div class="dropdown-form">
-                        <form class="form-default form-inline p15">
-                            <div class="input-group">
-                                <input type="text" class="form-control search-input" placeholder="Search...">
-                                <span class="input-group-btn"><button class="btn btn-rw btn-primary search-btn" type="button">Go!</button></span>
-                            </div>
-                        </form>
-                    </div>
-                </li>
-            </ul>
-        </li>
+        {{--<li class="dropdown visible-lg-block">--}}
+            {{--<a href="#" class="dropdown-toggle dropdown-form-toggle" data-toggle="dropdown"><i class="fa fa-search"></i></a>--}}
+            {{--<ul class="dropdown-menu dropdown-menu-user">--}}
+                {{--<li id="dropdownForm">--}}
+                    {{--<div class="dropdown-form">--}}
+                        {{--<form class="form-default form-inline p15">--}}
+                            {{--<div class="input-group">--}}
+                                {{--<input type="text" class="form-control search-input" placeholder="Search...">--}}
+                                {{--<span class="input-group-btn"><button class="btn btn-rw btn-primary search-btn" type="button">Go!</button></span>--}}
+                            {{--</div>--}}
+                        {{--</form>--}}
+                    {{--</div>--}}
+                {{--</li>--}}
+            {{--</ul>--}}
+        {{--</li>--}}
         {{-- /Search --}}
 
         <li class="navbar-social visible-lg-block"><a href="#" class="btn btn-social-icon btn-rw btn-primary btn-xs"><i class="fa fa-github-alt"></i></a></li>
