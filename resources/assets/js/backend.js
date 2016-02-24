bad304d004e10c7fc55885d7312d38e0b4f92ef3
tyloo <jbonva@gmail.com>
2016-02-24 11:16:51 +0100

    Updated dependencies and removed unused assets
    Signed-off-by: tyloo <jbonva@gmail.com>

--- resources/assets/js/backend.js
+++ resources/assets/js/backend.js
@@ -4,28 +4,28 @@ $(document).ready(function() {
     /* /Summernote */
 
     /* Bootstrap TagsInput */
-
-    var tags = new Bloodhound({
-        datumTokenizer: Bloodhound.tokenizers.obj.whitespace('name'),
-        queryTokenizer: Bloodhound.tokenizers.whitespace,
-        prefetch: {
-            url: '/admin/tags.json',
-            filter: function(list) {
-                return $.map(list, function(tag) {
-                    return { name: tag }; });
+    if ($('input[name="tags"]').length > 0) {
+        var tags = new Bloodhound({
+            datumTokenizer: Bloodhound.tokenizers.obj.whitespace('name'),
+            queryTokenizer: Bloodhound.tokenizers.whitespace,
+            prefetch: {
+                url: '/admin/tags.json',
+                filter: function(list) {
+                    return $.map(list, function(tag) {
+                        return { name: tag }; });
+                }
             }
-        }
-    });
-    tags.initialize();
+        });
+        tags.initialize();
 
-    $('input[name="tags"]').tagsinput({
-        typeaheadjs: {
-            name: 'tags',
-            displayKey: 'name',
-            valueKey: 'name',
-            source: tags.ttAdapter()
-        }
-    });
-
+        $('input[name="tags"]').tagsinput({
+            typeaheadjs: {
+                name: 'tags',
+                displayKey: 'name',
+                valueKey: 'name',
+                source: tags.ttAdapter()
+            }
+        });
+    }
     /* /Bootstrap TagsInput */
 });
