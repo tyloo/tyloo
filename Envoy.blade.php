09cf76e65ed5b6e519bf926224b5303b39d905ca
tyloo <jbonva@gmail.com>
2016-04-28 11:59:23 +0200

    Removed Envoy
    Signed-off-by: tyloo <jbonva@gmail.com>

--- Envoy.blade.php
+++
@@ -1,8 +0,0 @@
-@servers(['web' => 'tyloo@tyloo.fr'])
-
-@task('deploy', ['on' => 'web'])
-    cd /var/www/tyloo.fr
-    git pull origin master
-    composer install --no-dev -o
-    php artisan sitemap
-@endtask
