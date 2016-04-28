bbae0d4a2a6486d6af1c2a57d5e488ec8a133db4
tyloo <jbonva@gmail.com>
2016-04-28 11:58:05 +0200

    Added database test in config
    Signed-off-by: tyloo <jbonva@gmail.com>

--- config/database.php
+++ config/database.php
@@ -66,6 +66,20 @@ return [
             'engine'    => null,
         ],
 
+        'mysql_test' => [
+            'driver'    => 'mysql',
+            'host'      => env('DB_TEST_HOST', 'localhost'),
+            'port'      => env('DB_TEST_PORT', '3306'),
+            'database'  => env('DB_TEST_DATABASE', 'forge'),
+            'username'  => env('DB_TEST_USERNAME', 'forge'),
+            'password'  => env('DB_TEST_PASSWORD', ''),
+            'charset'   => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+            'prefix'    => '',
+            'strict'    => false,
+            'engine'    => null,
+        ],
+
         'pgsql' => [
             'driver'   => 'pgsql',
             'host'     => env('DB_HOST', 'localhost'),
