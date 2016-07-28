9a6038245127bccb74e302f8247ad0198c8e0432
Julien Bonvarlet <julien.bonvarlet@carvinglabs.com>
2016-07-28 10:45:26 -0400

    Applied fixes from StyleCI

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -7,13 +7,13 @@ Route::get('/', ['as' => 'pages.home', 'uses' => 'PagesController@home']);
 Route::get('resume', ['as' => 'pages.resume', 'uses' => 'PagesController@resume']);
 
 // Projects
-Route::group(['prefix' => 'projects'], function() {
+Route::group(['prefix' => 'projects'], function () {
     Route::get('/', ['as' => 'pages.projects.index', 'uses' => 'ProjectsController@index']);
     Route::get('{slug}', ['as' => 'pages.projects.show', 'uses' => 'ProjectsController@show']);
 });
 
 // Blog
-Route::group(['prefix' => 'blog'], function() {
+Route::group(['prefix' => 'blog'], function () {
     Route::get('/', ['as' => 'pages.blog.index', 'uses' => 'BlogController@index']);
     Route::get('{slug}', ['as' => 'pages.blog.show', 'uses' => 'BlogController@show']);
     Route::get('tags/{slug}', ['as' => 'pages.blog.tag', 'uses' => 'BlogController@tag']);
