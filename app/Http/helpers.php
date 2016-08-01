7a8f81fe30bb10588dee2c29016b45a6eec479f2
Julien Bonvarlet <julien.bonvarlet@carvinglabs.com>
2016-08-01 09:07:19 -0400

    Applied fixes from StyleCI

--- app/Http/helpers.php
+++ app/Http/helpers.php
@@ -1,7 +1,8 @@
 <?php
 
-if (! function_exists('active_state')) {
-    function active_state($path, $active = ' is-active') {
+if (!function_exists('active_state')) {
+    function active_state($path, $active = ' is-active')
+    {
         return call_user_func_array('Request::is', (array) $path) ? $active : '';
     }
 }
